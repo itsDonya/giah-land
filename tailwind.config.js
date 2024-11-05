542ad646832104735b0a5d6250c6c292f484f78f
2nya <thisis2nya@gmail.com>
2024-11-05 15:51:55 +0330

    core: setup base palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        primary: "#417F56",
+        "dark-primary": "#315F41",
+        "darker-primary": "#21402B",
+        "darkest-primary": "#102016",
+
+        secondary: "#B7D9D6",
+        "dark-secondary": "#87C1BB",
+        "darker-secondary": "#57A7A0",
+        "darkest-secondary": "#278F85",
+
+        error: "#ED2E2E",
+        "error-bg": "#FFF2F2",
       },
     },
   },
